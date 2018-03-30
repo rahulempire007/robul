934265f02539b49dcd6544e435e55327c143d46f
Rahul Saxena <saxena.rahul993@gmail.com>
2018-03-30 17:08:10 +0530

    test

--- app/src/main/java/instatag/com/b3ds/ListServices.java
+++ app/src/main/java/instatag/com/b3ds/ListServices.java
@@ -1,28 +1,32 @@
 package instatag.com.b3ds;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-/**
- * Created by haade on 13/09/2017.
- */
+
 
 public class ListServices extends Fragment {
 
     //Constructor default
-    public ListServices(){};
+    protected ServiceListViewAdapter serviceAdapter;
+    protected  ListView ls;
+    public ListServices(){}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View PageTwo = inflater.inflate(R.layout.ist_services_fragment, container, false);
-
-
+        ls=PageTwo.findViewById(R.id.servicelist);
+        //serviceAdapter=new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,serviceList);
+        //ls.setAdapter(serviceAdapter);
+        //Log.i(TAG, "onCreateView: ");
         return PageTwo;
     }
+
+
 
 }
