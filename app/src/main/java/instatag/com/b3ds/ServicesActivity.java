4e0c8896151ecba4760024763d4bddf42b0ac92e
Rahul Saxena <saxena.rahul993@gmail.com>
2018-03-30 17:10:41 +0530

    test

--- app/src/main/java/instatag/com/b3ds/ServicesActivity.java
+++ app/src/main/java/instatag/com/b3ds/ServicesActivity.java
@@ -1,8 +1,6 @@
 package instatag.com.b3ds;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -10,10 +8,22 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +31,10 @@ public class ServicesActivity extends AppCompatActivity {
 
     TabLayout MyTabs;
     ViewPager MyPage;
-
+    private AddServices addServices;
+    private ListServices listServices;
+    public static final MediaType JSON = MediaType.parse("application/json:charset=utf-8");
+    private ListServicesContact serviceResponse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,16 +51,101 @@ public class ServicesActivity extends AppCompatActivity {
         //Thanks For Watching and Keep Learning With ClipCodes on YouTube
     }
 
-    public void SetUpViewPager (ViewPager viewpage){
+    public void SetUpViewPager (final ViewPager viewpage){
         MyViewPageAdapter Adapter = new MyViewPageAdapter(getSupportFragmentManager());
 
-        Adapter.AddFragmentPage(new AddServices(), "ADD SERVICES");
-        Adapter.AddFragmentPage(new ListServices(), "LIST SERVICES");
+        addServices=new AddServices();
+        listServices=new ListServices();
+        Adapter.AddFragmentPage(addServices, "ADD SERVICES");
+        Adapter.AddFragmentPage(listServices, "LIST SERVICES");
 
         //We Need Fragment class now
 
         viewpage.setAdapter(Adapter);
+        viewpage.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            int i;
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
 
+            @Override
+            public void onPageSelected(int position) {
+                i=viewpage.getCurrentItem();
+                Log.i("Services Activity", "onPageSelected: "+i);
+                if(i==1)
+                {
+                    OkHttpClient client = new OkHttpClient();
+                    Request validation_request = listservices();
+                    client.newCall(validation_request).enqueue(new Callback() {
+
+                        @Override
+                        public void onFailure(Request request, IOException e) {
+
+                            // Toast.makeText(getApplicationContext(),"Fail",Toast.LENGTH_LONG).show();
+                            Log.i("Activity", "onFailure: Fail");
+                        }
+
+                        @Override
+                        public void onResponse(final Response response) throws IOException {
+                            listServiceResponse(response.body().string());
+                            final boolean isSuccessful=serviceResponse.getStatus();
+                            final List<Datum2> l=serviceResponse.getData();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (isSuccessful) {
+
+                                        Toast.makeText(getApplication(), "List of services ", Toast.LENGTH_SHORT).show();
+                                        ServiceListViewAdapter adapter=new ServiceListViewAdapter(getApplicationContext(),l);
+                                        listServices.ls.setAdapter(adapter);
+                                        //l.toArray();
+                                        /*for (int i=0;i<l.size();i++){
+                                            Datum2 d=(Datum2) l.get(i);
+                                            d.getService().
+                                        }*/
+                                        //new ListServices().serviceAdapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,
+                                        //       android.R.id.text1,l.toArray());
+                                        //Intent intent=new Intent(VerificationChoiceActivity.this,SetPasswordActivity.class);
+                                        //intent.putExtra("userid",userid);
+                                        //startActivity(intent);
+                                    } else {
+                                        Toast.makeText(getApplication(), "Could not load the list!!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                    });
+
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+
+    }
+    private Request listservices() {
+        JSONObject postdata = new JSONObject();
+        try {
+            postdata.put("user_id", MainActivity.userid);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(JSON, postdata.toString());
+        final Request request = new Request.Builder()
+                .addHeader("X-Api-Key", "AB5433GMDF657VBB")
+                .addHeader("Content-Type", "application/json")
+                .url("http://dev.feish.online/apis/listService")
+                .post(body)
+                .build();
+        return request;
+    }
+
+    public void listServiceResponse(String response) {
+        Gson gson = new GsonBuilder().create();
+        serviceResponse = gson.fromJson(response, ListServicesContact.class);
     }
 
     public class MyViewPageAdapter extends FragmentPagerAdapter{
