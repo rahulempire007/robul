4b9cb958c7e81502f524f7e9bd4e0a7a8f921734
Rahul Saxena <saxena.rahul993@gmail.com>
2018-04-07 01:31:30 +0530

    test

--- app/src/main/java/instatag/com/b3ds/MainActivity.java
+++ app/src/main/java/instatag/com/b3ds/MainActivity.java
@@ -3,6 +3,7 @@ package instatag.com.b3ds;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -36,6 +37,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Locale;
 
 import static java.security.AccessController.getContext;
 
@@ -51,14 +54,45 @@ public class MainActivity extends AppCompatActivity {
     String lusername,lpassword;
     private ProgressBar pb;
     protected static String userid;
+    protected static String user_type;
+
     public static final MediaType JSON
             = MediaType.parse("application/json; charset=utf-8");
     private String TAG="MainActivity";
 
+ //   @Override
+   // protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+     //   super.onActivityResult(requestCode, resultCode, data);
+   // }
+    public void getSpeechInput(View view) {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    luser.setText(result.get(0));
+                }
+                break;
+        }
     }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +112,9 @@ public class MainActivity extends AppCompatActivity {
         lpass=(EditText)findViewById(R.id.loginpassword);
         pb = (ProgressBar) findViewById(R.id.progress);
         pb.setVisibility(View.GONE);
+
+
+
 
         loginForEnter=(Button)findViewById(R.id.buttonforlogin);
         loginForEnter.setOnClickListener(new View.OnClickListener() {
@@ -113,6 +150,7 @@ public class MainActivity extends AppCompatActivity {
 
                                         Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_LONG).show();
                                         userid=loginResponse.getData().getId();
+                                        user_type=loginResponse.getData().getUserType();
 
 
                                         //Intent intent2=new Intent(MainActivity.this,DashboardActivity.class);
@@ -120,12 +158,12 @@ public class MainActivity extends AppCompatActivity {
                                         if((loginResponse.getData().getUserType()).compareTo("4")==0) {
 
                                             Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
-                                            intent.putExtra("firstname", loginResponse.getData().getFirstName());
-                                            intent.putExtra("mobile", loginResponse.getData().getMobile());
-                                            intent.putExtra("lastname", loginResponse.getData().getLastName());
-                                            intent.putExtra("email", loginResponse.getData().getEmail());
+                                           // intent.putExtra("firstname", loginResponse.getData().getFirstName());
+                                            //intent.putExtra("mobile", loginResponse.getData().getMobile());
+                                            //intent.putExtra("lastname", loginResponse.getData().getLastName());
+                                            //intent.putExtra("email", loginResponse.getData().getEmail());
                                             intent.putExtra("userid",loginResponse.getData().getId());
-                                           intent.putExtra("address",loginResponse.getData().getAddress().toString());
+                                           //intent.putExtra("address",loginResponse.getData().getAddress().toString());
 
 
                                             //intent.putExtra("userid", loginResponse.getData().getId());
@@ -135,15 +173,15 @@ public class MainActivity extends AppCompatActivity {
                                         else{
                                             Intent intent = new Intent(MainActivity.this, DoctorDashboardActivity.class);
                                             //intent.putExtra("mci_number", loginResponse.getData().getMciNumber().toString());
-                                            intent.putExtra("firstname", loginResponse.getData().getFirstName());
-                                            intent.putExtra("mobile", loginResponse.getData().getMobile());
-                                            intent.putExtra("lastname", loginResponse.getData().getLastName());
-                                            intent.putExtra("email", loginResponse.getData().getEmail());
+                                           // intent.putExtra("firstname", loginResponse.getData().getFirstName());
+                                            //intent.putExtra("mobile", loginResponse.getData().getMobile());
+                                            //intent.putExtra("lastname", loginResponse.getData().getLastName());
+                                            //intent.putExtra("email", loginResponse.getData().getEmail());
                                             intent.putExtra("userid", loginResponse.getData().getId());
-                                            intent.putExtra("qualification",(String)loginResponse.getData().getQualification());
-                                            intent.putExtra("consultationtime",loginResponse.getData().getConsultationTime());
-                                            intent.putExtra("identitytype",(String)loginResponse.getData().getIdentityType());
-                                           intent.putExtra("identityid",(String)loginResponse.getData().getIdentityId());
+                                            //intent.putExtra("qualification",(String)loginResponse.getData().getQualification());
+                                            //intent.putExtra("consultationtime",loginResponse.getData().getConsultationTime());
+                                            //intent.putExtra("identitytype",(String)loginResponse.getData().getIdentityType());
+                                           //intent.putExtra("identityid",(String)loginResponse.getData().getIdentityId());
 
 
                                             //intent2.putExtras(intent);
